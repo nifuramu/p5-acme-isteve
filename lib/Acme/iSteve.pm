87461a10b067166fac227249a05f4a6bb9fb123e
unk <nozzzzz@gmail.com>
2011-10-14 23:06:41 +0900

    initial commit

---
+++ lib/Acme/iSteve.pm
@@ -0,0 +1,32 @@
+package Acme::iSteve;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::iSteve -
+
+=head1 SYNOPSIS
+
+  use Acme::iSteve;
+
+=head1 DESCRIPTION
+
+Acme::iSteve is
+
+=head1 AUTHOR
+
+unk E<lt>nozzzzz@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
